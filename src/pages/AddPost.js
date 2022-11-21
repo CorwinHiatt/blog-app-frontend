0e293e5bfc890fab8fc898dce4b52df51a3f90a9
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-21 14:30:45 -0500

    cleaned up a lot

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -1,40 +1,42 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 export default function AddPost() {
-   const [form, setForm] = useState({})
+  const [form, setForm] = useState({})
 
+  const submitPost = e => {
+    e.preventDefault()
 
-   const submitPost = (e) => {
-      e.preventDefault()
-      fetch(`http://localhost:4040`, {
-         method: "POST",
-         headers: {
-        'content-type': 'application/json',
-         }, 
-         body: JSON.stringify(form),
+    fetch('http://localhost:4040', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+  }
 
-      })
-   }
+  const handleForm = e => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
-   const handleForm = (e) => {
-      setForm({...form, [e.target.name]: e.target.value})
-   }
+  console.log(form)
 
-  return(
- 
-
-    <div className="container">
+  return (
+    <div className='container'>
       <h1>Add Post</h1>
-      <form action="submit"n>
-        <label htmlFor="">Author</label>
-        <input  name='author'type="text" onChange={handleForm} />
-        <label htmlFor="">Text</label>
-        <input name='text 'type="text" onChange={handleForm}/>
-        <label htmlFor="">Date</label>
-        <input name='date' type="date" onChange={handleForm} />
-       
-        <button type="submit" onClick={submitPost}>Submit</button>
-
+      <form action='submit'>
+        <label htmlFor=''>Author</label>
+        <input name='author' type='text' onChange={handleForm} />
+        <br />
+        <label htmlFor=''>Text</label>
+        <input name='text' type='text' onChange={handleForm} />
+        <br />
+        <label htmlFor=''>Date</label>
+        <input name='date' type='date' onChange={handleForm} />
+        <br />
+        <button type='submit' onClick={submitPost}>
+          Add new post
+        </button>
       </form>
     </div>
   )
